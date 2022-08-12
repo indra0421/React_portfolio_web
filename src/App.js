ed90c68b8ac79eb137657717f87478377aa4614e
Indranil Naskar <82460475+indra0421@users.noreply.github.com>
2022-08-12 23:15:13 +0530

    portfolio_sec

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Intro} from './Components/Intro/Intro'
 import {Services} from './Components/Services/Services'
 import Experience from './Components/Experience/Experience'
 import {Works} from './Components/Works/Works'
+import { Portfolio } from './Components/Portfolio/Portfolio'
 const App = () => {
   return (
     <div className='App'>
@@ -13,6 +14,7 @@ const App = () => {
       <Services/>
       <Experience/>
       <Works/>
+      <Portfolio/>
       </div>
   );
 }
---
+++ src/Components/Portfolio/Portfolio.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Portfolio.css'
+import {Swiper,SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+
+import siderbar from '../../img/sidebar.png'
+import ecommerce from '../../img/ecommerce.png'
+import hoc from '../../img/hoc.png'
+import musicapp from '../../img/musicapp.png'
+
+
+export const Portfolio = () => {
+  return (
+    <div className='portfolio'>
+        {/* heading */}
+        <span>Recent Projects</span>
+        <span>Portfolio</span>
+
+        {/* slider */}
+        <Swiper 
+        spaceBetween={30}
+        slidesPerView={3}
+        grabCursor = {true}
+        className = 'portfolio-slider'>
+           
+           <SwiperSlide className='swiper-slide'>
+             <img src={siderbar} alt="" />
+           </SwiperSlide>  
+           <SwiperSlide className='swiper-slide'>
+             <img src={musicapp} alt="" />
+           </SwiperSlide>  
+           <SwiperSlide className='swiper-slide'>
+             <img src={hoc} alt="" />
+           </SwiperSlide>  
+           <SwiperSlide className='swiper-slide'>
+             <img src={ecommerce} alt="" />
+           </SwiperSlide>  
+             
+
+
+        </Swiper>
+    </div>
+  )
+}
+
